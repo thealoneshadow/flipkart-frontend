d2b98d1e21c0cd591ef17cb5e8ae46ec2b25420f
TheAloneShadow <amayauta12@gmail.com>
2023-01-27 18:23:46 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,6 @@
 /** @format */
 
-export const api = "http://localhost:2000/api";
+export const api = "https://flipkart-backend.netlify.app/.netlify/functions/api";
 export const generatePublicUrl = (fileName) => {
 	return `http://localhost:2000/public/${fileName}`;
 };
