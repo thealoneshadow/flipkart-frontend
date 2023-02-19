e1faa2c58809eb3c6f4b4636d20b4e9c6af4c2ca
TheAloneShadow <amayauta12@gmail.com>
2023-02-19 22:50:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 /** @format */
 
 import "./App.css";
+import HomePage from "./container/HomePage/HomePage";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes as Switch,
+} from "react-router-dom";
+import ProductList from "./container/ProductListPage/ProductListPage";
+import { useEffect } from "react";
+import { isUserLoggedIn, updateCart } from "./actions";
+import { useDispatch, useSelector } from "react-redux";
+import ProductDetailsPage from "./container/ProductDetailsPage/ProductDetailsPage";
+import CartPage from "./container/CartPage/CartPage";
+import CheckoutPage from "./container/CheckoutPage/CheckoutPage";
+import OrderPage from "./container/OrderPage/OrderPage";
+import OrderDetailsPage from "./container/OrderDetailsPage/OrderDetailsPage";
+import {
+	getProductsByCategory,
+	getProductsBySlug,
+} from "./actions/product.action";
+import NotFound from "./container/NotFound/NotFound";
 function App() {
+	const dispatch = useDispatch();
+	const auth = useSelector((state) => state.auth);
+	useEffect(() => {
+		if (!auth.authenticate) {
+			dispatch(isUserLoggedIn());
+		}
+	}, [auth.authenticate]);
+	useEffect(() => {
+		dispatch(updateCart());
+	}, [auth.authenticate]);
 
 	return (
 		<div className="App">
