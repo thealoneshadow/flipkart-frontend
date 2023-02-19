c2874a7a2d784a52f1441563bbd41d2a445b593f
TheAloneShadow <amayauta12@gmail.com>
2023-02-19 22:05:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>Hello I am running</h1>
+			<Router>
+				<Switch>
+					<Route path="/" exact element={<HomePage />} />
+					<Route path="/cart" element={<CartPage />} />
+					<Route path="/checkout" element={<CheckoutPage />} />
+					<Route path="/account/orders" element={<OrderPage />} />
+					<Route
+						path="/order_details/:orderId"
+						element={<OrderDetailsPage />}
+					/>
+					<Route
+						path="/:productSlug/:productId/page"
+						element={<ProductDetailsPage />}
+					/>
+					<Route path="/:slug" element={<ProductList />} />
+					<Route path="/*" element={<NotFound />} />
+				</Switch>
+			</Router>
 		</div>
 	);
 }
